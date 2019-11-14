d34b233bb063d08b59d0aa25b961b30c4b11ee3b
cnliujianfeng <2214414345@qq.com>
2019-11-14 16:17:35 +0800

    1.使用spring—validator进行服务端验证

--- my-shop-domain/src/main/java/com/ljf/my/shop/domain/TbUser.java
+++ my-shop-domain/src/main/java/com/ljf/my/shop/domain/TbUser.java
@@ -2,9 +2,14 @@ package com.ljf.my.shop.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ljf.my.shop.commons.persistence.BaseEntity;
+import com.ljf.my.shop.commons.utils.RegexpUtils;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -20,9 +25,14 @@ import java.util.Date;
 @EqualsAndHashCode(callSuper = true)
 public class TbUser extends BaseEntity {
 
+    @Length(min = 6,max = 20,message = "姓名的长度必须介于6-20位之间")
     private String username;
+    @JsonIgnore
+    @Length(min = 6,max = 30,message = "密码长度必须设置为6-20位")
     private String password;
+    @Pattern(regexp = RegexpUtils.PHONE,message = "手机号格式不正确")
     private String phone;
+    @Pattern(regexp = RegexpUtils.EMAIL,message = "邮箱格式不正确")
     private String email;
 
 
